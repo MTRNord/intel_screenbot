f9d285a35432e9b9c474d82784fb6170381227f6
Marcel <MTRNord@users.noreply.github.com>
2016-11-13 01:19:04 +0100

    Fix Plugins

--- __init__.py
+++ __init__.py
@@ -243,24 +243,18 @@ def iitc(bot, event, *args):
         logger.debug("temporary screenshot file: {}".format(filepath))
         
         if bot.conversation_memory_get(event.conv_id, 'iitc_plugins'):
+            plugins = []
             plugin_names = bot.conversation_memory_get(event.conv_id, 'iitc_plugins').split(", ")
             if bot.memory.exists(["iitc_plugins"]):
-                plugins = []
                 for plugin_objects in bot.memory.get_by_path(["iitc_plugins"]):
-                    list = []
-                    for attribute, value in plugin_objects.items():
-                        for plugin_name in plugin_names:
-                            if attribute  == "url":
-                                list.append(value)
-                            if attribute  == "name":
-                                if value == plugin_name:
-                                    plugin = list[0]
-                                    plugins.append(value)
+                    for plugin_name in plugin_names:
+                        if plugin_objects["name"]  == plugin_name:
+                            plugins.append(list["url"])
         else:
-             plugin = ''
+             plugins = ''
         try:
             loop = asyncio.get_event_loop()
-            image_data = yield from _screencap("iitc", url, filepath, filename, SACSID, CSRF, plugin, search, bot, event)
+            image_data = yield from _screencap("iitc", url, filepath, filename, SACSID, CSRF, plugins, search, bot, event)
         except Exception as e:
             yield from bot.coro_send_message(event.conv_id, "<i>error getting screenshot</i>")
             logger.exception("screencap failed".format(url))
