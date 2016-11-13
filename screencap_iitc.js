cd55c5cc585efaa8fd4574408a5e6e89e4dc7800
Marcel <MTRNord@users.noreply.github.com>
2016-11-13 03:31:52 +0100

    Remove function not needed for iitc

--- screencap_iitc.js
+++ screencap_iitc.js
@@ -61,46 +61,6 @@ function addCookies(sacsid, csrf) {
   });
 }
 
-function setMinMax(min, max) {
-  var minAvailable = page.evaluate(function() {
-    return document.querySelectorAll('.level_notch.selected')[0];
-  });
-  if (parseInt(minAvailable.id[9], 10) > min) {
-  } else {
-    var rect = page.evaluate(function() {
-      return document.querySelectorAll('.level_notch.selected')[0].getBoundingClientRect();
-    });
-    page.sendEvent('click', rect.left + rect.width / 2, rect.top + rect.height / 2);
-    window.setTimeout(function() {
-      var rect1 = page.evaluate(function(min) {
-        return document.querySelector('#level_low' + min).getBoundingClientRect();
-      }, min);
-      page.sendEvent('click', rect1.left + rect1.width / 2, rect1.top + rect1.height / 2);
-      if (max === 8) {
-        page.evaluate(function() {
-          document.querySelector('#filters_container').style.display = 'none';
-        });
-      }
-    }, 2000);
-  }
-  if (max < 8) {
-    window.setTimeout(function() {
-      var rect2 = page.evaluate(function() {
-        return document.querySelectorAll('.level_notch.selected')[1].getBoundingClientRect();
-      });
-      page.sendEvent('click', rect2.left + rect2.width / 2, rect2.top + rect2.height / 2);
-      window.setTimeout(function() {
-        var rect3 = page.evaluate(function(min) {
-          return document.querySelector('#level_high' + min).getBoundingClientRect();
-        }, max);
-        page.sendEvent('click', rect3.left + rect3.width / 2, rect3.top + rect3.height / 2);
-        page.evaluate(function() {
-          document.querySelector('#filters_container').style.display = 'none';
-        });
-      }, 2000);
-    }, 4000);
-  }
-}
 
 /**
  * Does all stuff needed after cookie authentication
@@ -148,13 +108,11 @@ function afterCookieLogin(IntelURL, search) {
                 });
             },
             success: function () {
-                setMinMax(1, 8);
                 hideDebris();
                 prepare('1920', '1080', search);
                 main();
             },
             error: function () {
-                setMinMax(1, 8);
                 hideDebris();
                 prepare('1920', '1080', search);
                 main();
