2ccc78af70640ab8815aa3e1e75ad72b0da4e1fe
Marcel <MTRNord@users.noreply.github.com>
2016-11-13 01:57:51 +0100

    Don't send it as string

--- __init__.py
+++ __init__.py
@@ -98,12 +98,12 @@ def _screencap(maptype, url, filepath, filename, SACSID, CSRF, plugins, search, 
             exitcode, stdout = yield from task
     else:
         if search == False:
-            command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" "' + plugins + '"'
+            command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" ' + plugins
             task = _get_lines(command)
             task = asyncio.wait_for(task, 180.0, loop=self.loop)
             exitcode, stdout = loop.run_until_complete(task)
         else:
-            command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" "' + search + '" "' + plugins + '"'
+            command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" "' + search + '" ' + plugins
             task = _get_lines(command)
             task = asyncio.wait_for(task, 180.0, loop=loop)
             exitcode, stdout = yield from task
