3f9216515906e56bb3db1f561c0a65de9d40e5c1
Marcel <MTRNord@users.noreply.github.com>
2016-11-14 20:37:47 +0100

    Adjust timeouts

--- __init__.py
+++ __init__.py
@@ -92,23 +92,23 @@ def _screencap(maptype, url, filepath, filename, SACSID, CSRF, plugins, search, 
         if search == False:
             command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '"'
             task = _get_lines(command)
-            task = asyncio.wait_for(task, 180.0, loop=self.loop)
+            task = asyncio.wait_for(task, 240.0, loop=self.loop)
             exitcode, stdout = loop.run_until_complete(task)
         else:
             command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" "' + search + '"'
             task = _get_lines(command)
-            task = asyncio.wait_for(task, 180.0, loop=loop)
+            task = asyncio.wait_for(task, 240.0, loop=loop)
             exitcode, stdout = yield from task
     else:
         if search == False:
             command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" "' + plugins + '"'
             task = _get_lines(command)
-            task = asyncio.wait_for(task, 180.0, loop=self.loop)
+            task = asyncio.wait_for(task, 240.0, loop=self.loop)
             exitcode, stdout = loop.run_until_complete(task)
         else:
             command = 'phantomjs hangupsbot/plugins/intel_screenbot/screencap_' + maptype + '.js "' + SACSID + '" "' + CSRF + '" "' + url + '" "' + filepath + '" "' + search + '" "' + plugins + '"'
             task = _get_lines(command)
-            task = asyncio.wait_for(task, 180.0, loop=loop)
+            task = asyncio.wait_for(task, 240.0, loop=loop)
             exitcode, stdout = yield from task
 
     # read the resulting file into a byte array
