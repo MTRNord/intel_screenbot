0c2183a2ac29ce02becc86711af8ba24f3d253c4
Marcel <MTRNord@users.noreply.github.com>
2016-11-15 19:25:23 +0100

    Move clear localstorage

--- screencap_iitc.js
+++ screencap_iitc.js
@@ -89,12 +89,12 @@ function storeCookies() {
 function firePlainLogin(SACSID, CSRF) {
   console.log("firePlainLogin")  
   page.open('https://www.ingress.com/intel', function (status) {
+    page.evaluate(function () {
+      localStorage.clear()
+    });
     if (status !== 'success') {quit('unable to connect to remote server')}
     var link = 'https://www.google.com/accounts/ServiceLogin?service=ah&passive=true&continue=https://appengine.google.com/_ah/conflogin%3Fcontinue%3Dhttps://www.ingress.com/intel&ltmpl='
     page.open(link, function () {
-      page.evaluate(function () {
-        localStorage.clear()
-      });
       login(SACSID, CSRF);
     });
   });
